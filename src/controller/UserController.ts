e74efdfac39a814e55b5c7981f6fddd64dcc3b0d
eastflag <eastflag@gmail.com>
2019-05-13 02:52:48 +0900

    rest-node tutorial 5 - controller module

---
+++ src/controller/UserController.ts
@@ -0,0 +1,5 @@
+export class UserController {
+  static getHeroes = (req, res) => {
+    res.send('hero list');
+  }
+}
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,9 +1,8 @@
 import {Router} from 'express';
+import {UserController} from "../controller/UserController";
 
 const routes = Router();
 
-routes.get('/heroes', (req, res) => {
-  res.send('heroes list');
-});
+routes.get('/heroes', UserController.getHeroes);
 
 export default routes;
