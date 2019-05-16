957a89488202ad978e8185f33fff320b1922adbe
eastflag <eastflag@gmail.com>
2019-05-17 02:08:13 +0900

    rest-node tutorial 8 - typeorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import express from 'express';
 import router from './router';
 import bodyParser from 'body-parser';
+import {createConnection} from "typeorm";
+import {Photo} from "./entity/Photo";
 
 const app = express();
 
-app.get('/api/hello', (req, res) => {
-  res.send('hello world');
-});
+createConnection(/*...*/).then(async connection => {
 
-app.use(bodyParser());
+  let photo = new Photo();
+  photo.name = "Me and Bears";
+  photo.description = "I am near polar bears";
+  photo.filename = "photo-with-bears.jpg";
+  photo.views = 1;
+  photo.isPublished = true;
 
-app.use('/api', router);
+  await connection.manager.save(photo);
+  console.log("Photo has been saved");
 
-app.listen(8000, () => {
-  console.log('server is listening 8000');
-});
+  // start express server --------------------------------
+  app.use(bodyParser());
+
+  app.use('/api', router);
+
+  app.listen(8000, () => {
+    console.log('server is listening 8000');
+  });
+
+}).catch(error => console.log(error));
+
