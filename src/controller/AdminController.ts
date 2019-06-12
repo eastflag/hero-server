4d1bcede9395545c70f5f08997991182748da3db
eastflag <eastflag@gmail.com>
2019-06-12 14:55:02 +0900

    post /api/admin/hero 히어로 등록

--- src/controller/AdminController.ts
+++ src/controller/AdminController.ts
@@ -1,6 +1,32 @@
+import {Hero} from "../entity/Hero";
+import {getConnection} from "typeorm";
+import {Power} from "../entity/Power";
+import {ResultVo} from "../vo/ResultVo";
+
 export class AdminController {
-  static addHero = (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+  static addHero = async (req, res) => {
+    const {name, email, sex, country, address, photo, powers} = req.body;
+
+    const newHero = new Hero();
+    newHero.name = name;
+    newHero.email = email;
+    newHero.sex = sex;
+    newHero.country = country;
+    newHero.address = address;
+    newHero.photo = photo;
+    await getConnection().getRepository(Hero).save(newHero);
+
+    if (powers && powers.length > 0) {
+      const newPowers = powers.map(power => {
+        const p = new Power();
+        p.name = power;
+        p.hero = newHero;  // relation key
+        return p;
+      })
+      // bulk insert
+      await getConnection().createQueryBuilder().insert().into(Power).values(newPowers).execute();
+    }
+
+    res.send(new ResultVo(0, 'success'));
   }
 }
